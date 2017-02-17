0d5f52d819f513374a8f13ce6f908ee96fbb5459
ysden123 <ysden123@gmail.com>
2017-02-17 23:34:17 +0200

    One class

---
+++ src/main/scala/com/stulsoft/npm/analyzer/Analyzer.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+
+package com.stulsoft.npm.analyzer
+
+import java.io.File
+
+import org.json4s.native.JsonMethods.parse
+
+import scala.io.Source
+
+/**
+  * @author Yuriy Stul
+  */
+object Analyzer extends App {
+  /**
+    * Gets a collection of the dependencies
+    *
+    * @param path full path to file (package.json)
+    * @return the collection of the dependencies
+    */
+  def getDependencies(path: String): Iterable[String] = {
+    val file = new File(path)
+    val json = parse(file)
+    try {
+      val dependencies = json.values.asInstanceOf[Map[String, AnyVal]]("dependencies")
+      dependencies.asInstanceOf[Map[String, String]].keys
+    }
+    catch {
+      case _: Exception => Set[String]()
+    }
+  }
+  /**
+    * Returns name of module, if it was defined
+    *
+    * @param text the test text
+    * @return name of module, if it was defined; None - otherwise
+    */
+  def getModuleName(text: String): Option[String] = {
+    text match {
+      case null => None
+      case s: String if s.isEmpty => None
+      case s: String =>
+        val pattern = "require\\([',\"](.*)[',\"]\\)".r
+        for (m <- pattern findFirstMatchIn s) yield m.group(1)
+    }
+  }
+  /**
+    * Returns an array of the JavaScript files.
+    *
+    * @param dir start directory
+    * @return array of the JavaScript files.
+    */
+  def getJSFiles(dir: File): Array[File] = {
+    require(dir != null, "Parameter dir could not be null.")
+    require(dir.exists(), s"Directory ${dir.getName} doesn't exist.")
+    val these = dir.listFiles.filter(_.getName.endsWith(".js"))
+    these ++ dir.listFiles.filter(f => f.isDirectory && !f.getName.contains("node_modules")).flatMap(getJSFiles)
+  }
+
+  private def getUsedModuleNames(files: Array[File]): Set[String] = {
+    files
+      .map(file => Source.fromFile(file).getLines()
+        .map(getModuleName)
+        .flatten)
+      .flatMap(_.toSeq)
+      .toSet
+  }
+
+  def getUnusedModuleNames(path: String): Set[String] = {
+    val usedModuleNames = getUsedModuleNames(getJSFiles(new File(path)))
+
+    val declaredModuleNames = getDependencies(new File(path, "package.json").getPath)
+    declaredModuleNames.filter(dm => !usedModuleNames.contains(dm)).toSet
+  }
+
+  require(args != null && args.length >= 1, "Path to npm project is not specified.")
+  val projectPath = new File(args(0))
+  require(projectPath.exists() && projectPath.isDirectory, s"Wrong path to npm project: ${args(0)}")
+
+  val unusedModuleNames = getUnusedModuleNames(args(0))
+  println(s"Number of unused modules is ${unusedModuleNames.size}")
+
+  if (unusedModuleNames.nonEmpty) {
+    println("List of unused modules:")
+    unusedModuleNames.foreach(println)
+  }
+}
---
+++ src/test/scala/com/stulsoft/npm/analyzer/AnalyzerTest.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+
+package com.stulsoft.npm.analyzer
+
+import java.io.File
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Unit tests for Analyzer class
+  *
+  * @author Yuriy Stul
+  */
+class AnalyzerTest extends FlatSpec with Matchers {
+  behavior of "AnalyzerTest"
+
+  "getModuleName" should "return None for incorrect input" in {
+    Analyzer.getModuleName(null) should equal(None)
+    Analyzer.getModuleName("") should equal(None)
+  }
+
+  it should "return module name" in {
+    Analyzer.getModuleName("require('test')") should equal(Some("test"))
+    Analyzer.getModuleName("""require("test")""") should equal(Some("test"))
+    Analyzer.getModuleName(""" require("test").""") should equal(Some("test"))
+    Analyzer.getModuleName(""" new (require("test")).""") should equal(Some("test"))
+    Analyzer.getModuleName(""" abc require("test")()""") should equal(Some("test"))
+  }
+
+  "getDependencies" should "get dependencies from package.json filr" in {
+    val dependencies = Analyzer.getDependencies(getClass.getResource("/package.json").toURI.getPath)
+    dependencies.size should equal(2)
+    dependencies.head should equal("hazelcast-client")
+    dependencies.tail.head should equal("async")
+  }
+
+
+  "getJSFiles" should "return list of JavaScript files" in {
+    val testProjectPath = getClass.getResource("/test-project").toURI.getPath
+    val files = Analyzer.getJSFiles(new File(testProjectPath))
+    files.length > 0 should equal(true)
+    files.exists(_.getName.endsWith("m1.js")) should equal(true)
+    files.exists(_.getName.endsWith("m2.js")) should equal(true)
+    files.exists(_.getName.endsWith("ms1.js")) should equal(true)
+  }
+
+  it should "handle error" in {
+    assertThrows[IllegalArgumentException] {
+      Analyzer.getJSFiles(null)
+    }
+
+    assertThrows[IllegalArgumentException] {
+      Analyzer.getJSFiles(new File("ERROR22ERROR"))
+    }
+  }
+
+  "getUnusedModuleNames" should "get list of used module names" in {
+    val unusedModuleNames = Analyzer.getUnusedModuleNames(getClass.getResource("/test-project").toURI.getPath)
+    unusedModuleNames should not be null
+    unusedModuleNames.size should equal(1)
+  }
+
+  "main" should "prevent incorrect program parameters" in {
+    assertThrows[IllegalArgumentException] {
+      Analyzer.main(null)
+    }
+    assertThrows[IllegalArgumentException] {
+      Analyzer.main(Array("error"))
+    }
+    assertThrows[IllegalArgumentException] {
+      Analyzer.main(Array("package.json"))
+    }
+  }
+}
